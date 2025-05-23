4404d1196ea6e3bcdee90f60405c1d6fe5ff5540
calimeira <meiracali95@gmail.com>
2025-05-23 17:54:59 -0300

    actualizacion hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,15 +4,14 @@ import bgImage from '../assets/hero.png';
 
 export default function Hero() {
   return (
-    <section
-      className="hero"
-      style={{ backgroundImage: `url(${bgImage})` }}
-    >
-   {/*   <h1 className="hero__title">Bienvenido a AllenGroup Insurance</h1>
-      <p className="hero__subtitle">
-        Protege tu futuro hoy con nuestros seguros personalizados.
-      </p>
-      <button className="hero__cta">Cotiza ahora</button>*/}
+    <section className="hero">
+      <div className="hero__banner">
+        Brindamos soluciones de
+        <span className="hero__highlight-light"> seguros ágiles y personalizadas </span>,
+        conectando a las personas con una protección
+        <span className="hero__highlight-blue"> real y confiable.</span>
+      </div>
+      <img src={bgImage} alt="Hero" className="hero__image" />
     </section>
   );
 }
